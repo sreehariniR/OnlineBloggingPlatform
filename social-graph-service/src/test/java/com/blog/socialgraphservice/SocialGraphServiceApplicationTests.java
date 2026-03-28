59e410cb5e1c407e547842770314ea411a9fdc81
sreehariniR <harinisree.ramesh@gmail.com>
2026-03-28 19:16:48 +0530

    Initial project structure2

---
+++ social-graph-service/src/test/java/com/blog/socialgraphservice/SocialGraphServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.socialgraphservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialGraphServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
